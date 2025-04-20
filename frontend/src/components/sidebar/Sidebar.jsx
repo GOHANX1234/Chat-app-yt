3b7b96b874b5e31370c1638d005f1ff46090b7e0
GOHANX1234 <francee397@gmail.com>
2025-04-20 16:06:40 +0530

    Add files via upload

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -3,30 +3,36 @@ import LogoutButton from "./LogoutButton";
 import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
-	return (
-		<div className='border-r border-slate-500 p-4 flex flex-col'>
-			<SearchInput />
-			<div className='divider px-3'></div>
-			<Conversations />
-			<LogoutButton />
-		</div>
-	);
+  return (
+    <aside
+      className="
+        w-full        /* full width on mobile */
+        sm:w-72       /* 18rem wide on tablet+ */
+        h-screen      /* full viewport height */
+        flex flex-col
+        bg-white bg-opacity-10 backdrop-blur-lg
+        border-r border-white border-opacity-20
+      "
+    >
+      {/* Search at top */}
+      <div className="p-4">
+        <SearchInput />
+      </div>
+
+      {/* Divider */}
+      <hr className="border-white border-opacity-20" />
+
+      {/* Scrollable conversation list */}
+      <div className="flex-1 overflow-y-auto p-4 space-y-2">
+        <Conversations />
+      </div>
+
+      {/* Logout at bottom */}
+      <div className="p-4">
+        <LogoutButton />
+      </div>
+    </aside>
+  );
 };
+
 export default Sidebar;
-
-// STARTER CODE FOR THIS FILE
-// import Conversations from "./Conversations";
-// import LogoutButton from "./LogoutButton";
-// import SearchInput from "./SearchInput";
-
-// const Sidebar = () => {
-// 	return (
-// 		<div className='border-r border-slate-500 p-4 flex flex-col'>
-// 			<SearchInput />
-// 			<div className='divider px-3'></div>
-// 			<Conversations />
-// 			<LogoutButton />
-// 		</div>
-// 	);
-// };
-// export default Sidebar;
